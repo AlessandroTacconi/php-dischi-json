12a216fcba45d99fc3b97165c5de784f2f717e17
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2024-01-11 17:31:50 +0100

    feat: cards

--- index.php
+++ index.php
@@ -10,20 +10,27 @@
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
       crossorigin="anonymous"
     />
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <div id="app">
+    <div id="app" v-cloak>
       <header class="text-center my-3">
         <h1>I miei dischi</h1>
       </header>
-      <main>
-        <ul>
-          <li v-for="disc in discs">
-            <div>{{ disc.nome_album }}</div>
-            <div>{{ disc.artista }}</div>
-            <div>{{ disc.anno }}</div>
-          </li>
-        </ul>
+        <main class="container">
+          <div class="row">
+            <div v-for="disc in discs" class="col-md-4">
+              <div class="card">
+                <img :src="disc.cover" :alt="disc.nome_album">
+                <div class="card-body">
+                  <div>{{ disc.nome_album }}</div>
+                  <div>{{ disc.artista }}</div>
+                  <div>{{ disc.anno }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
@@ -31,3 +38,6 @@
     <script src="index.js"></script>
   </body>
 </html>
+
+
+
